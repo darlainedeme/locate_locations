0cf7194b1b3d0dd871ce36a338456f45cb42c03b
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-07-05 10:20:14 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -14,27 +14,26 @@ import streamlit as st
 
 # uploaded_file = 'All_coal_plant_OPR_processed.csv'
 uploaded_file = st.file_uploader('upload csv file')
-
-
-coal_plants = pd.read_csv(uploaded_file, encoding = 'unicode_escape')
-n = 10
-i = 0
-#wb = load_workbook(filepath)
-#sheet = wb["Envision"]
-#wb["Elec_Sub"]
-#cell = sheet.cell(2,20)
-for index, row in coal_plants[0:n].iterrows():
-    i += 1
-    if row['B'] is not np.nan:
-        sentence = row['B'] + ' ' + row['STATE'] + ' ' + row['COUNTRY']
-    else:
-        sentence = row['A'] + ' ' + row['STATE'] + ' ' + row['COUNTRY']
-    url = 'https://nominatim.openstreetmap.org/search?query=' + quote(sentence)
-    resp = requests.request("Get",url)
-    if (resp.status_code != 404) :
-        strRes = resp.text
-        root = ET.fromstring(strRes)
-
-    st.write(i)
-    st.write(location)
-    st.write('-------')
+if uploaded_file is not None:
+    coal_plants = pd.read_csv(uploaded_file, encoding = 'unicode_escape')
+    n = 10
+    i = 0
+    #wb = load_workbook(filepath)
+    #sheet = wb["Envision"]
+    #wb["Elec_Sub"]
+    #cell = sheet.cell(2,20)
+    for index, row in coal_plants[0:n].iterrows():
+        i += 1
+        if row['B'] is not np.nan:
+            sentence = row['B'] + ' ' + row['STATE'] + ' ' + row['COUNTRY']
+        else:
+            sentence = row['A'] + ' ' + row['STATE'] + ' ' + row['COUNTRY']
+        url = 'https://nominatim.openstreetmap.org/search?query=' + quote(sentence)
+        resp = requests.request("Get",url)
+        if (resp.status_code != 404) :
+            strRes = resp.text
+            root = ET.fromstring(strRes)
+    
+        st.write(i)
+        st.write(location)
+        st.write('-------')
